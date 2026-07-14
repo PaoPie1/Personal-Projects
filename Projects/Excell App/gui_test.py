dd638b1ec88622e0992dca90b03e85b5bac8d398
PaoPie1 <gabrielwilliam0118@gmail.com>
2026-07-14 11:29:58 +0800

    first commit

---
+++ Projects/Excell App/gui_test.py
@@ -0,0 +1,74 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTabWidget, QLabel, QVBoxLayout, QWidget, QLineEdit, QPushButton
+
+app = QApplication(sys.argv)
+window = QMainWindow()
+
+window.setWindowTitle("Stainless Shop POS")
+window.resize(900, 700)
+
+
+tabs = QTabWidget()
+
+tab1 = QWidget()
+tab2 = QWidget()
+tab3 = QWidget()
+tab4 = QWidget()
+
+tabs.addTab(tab1, "Add Item")
+tabs.addTab(tab2, "New Sale")
+tabs.addTab(tab3, "Stock List")
+tabs.addTab(tab4, "Deliveries")
+
+
+
+
+
+layout1 = QVBoxLayout()
+
+name_label = QLabel("Product Unit Name:")
+name_input = QLineEdit()
+
+cost_label = QLabel("Product Cost:")
+cost_input = QLineEdit()
+
+sell_label = QLabel("Product Selling Price:")
+sell_input = QLineEdit()
+
+stock_label = QLabel("Stock Available:")
+stock_input = QLineEdit()
+
+save_button = QPushButton("Save Product")
+
+
+layout1.addWidget(name_label)
+layout1.addWidget(name_input)
+
+layout1.addWidget(cost_label)
+layout1.addWidget(cost_input)
+
+layout1.addWidget(sell_label)
+layout1.addWidget(sell_input)
+
+layout1.addWidget(stock_label)
+layout1.addWidget(stock_input)
+
+layout1.addWidget(save_button)
+
+tab1.setLayout(layout1)
+
+
+
+
+
+
+
+
+
+
+window.setCentralWidget(tabs)
+window.show()
+
+sys.exit(app.exec())
+
+
