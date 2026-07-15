e9c6e3ad9c6e0f9cb107041ac1cfba8e9d914ab6
PaoPie1 <gabrielwilliam0118@gmail.com>
2026-07-15 21:28:48 +0800

    1st change to classes

--- Excell App/gui_test.py
+++ Excell App/gui_test.py
@@ -1,70 +1,41 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTabWidget, QLabel, QVBoxLayout, QWidget, QLineEdit, QPushButton
 
-app = QApplication(sys.argv)
-window = QMainWindow()
 
-window.setWindowTitle("Stainless Shop POS")
-window.resize(900, 700)
+class StainlessApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Stainless Shop POS")
+        self.resize(900, 600)
 
+        self.setup_ui()
 
-tabs = QTabWidget()
+    def setup_ui(self):
+        tabs = QTabWidget()
 
-tab1 = QWidget()
-tab2 = QWidget()
-tab3 = QWidget()
-tab4 = QWidget()
+        self.tab1 = QWidget()
+        tab2 = QWidget()
+        tab3 = QWidget()
+        tab4 = QWidget()
 
-tabs.addTab(tab1, "Add Item")
-tabs.addTab(tab2, "New Sale")
-tabs.addTab(tab3, "Stock List")
-tabs.addTab(tab4, "Deliveries")
+        tabs.addTab(self.tab1, "Add Item")
+        tabs.addTab(tab2, "New Sale")
+        tabs.addTab(tab3, "Stock List")
+        tabs.addTab(tab4, "Deliveries")
 
+        layout1 = QVBoxLayout()
 
+        self.name_label = QLabel("Product Unit Name:")
+        self.name_input = QLineEdit()
 
-def add_prod_to_db():
-    name = name_input.text()
-    cost = cost_input.text()
-    sell = sell_input.text()
-    stock = stock_input.text()
-    print(f"Saved! {name, cost, sell, stock}")
+        self.cost_label = QLabel("Product Cost:")
+        self.cost_input = QLineEdit()
 
+        self.sell_label = QLabel("Product Selling Price:")
+        self.sell_input = QLineEdit()
 
-
-layout1 = QVBoxLayout()
-
-name_label = QLabel("Product Unit Name:")
-name_input = QLineEdit()
-
-cost_label = QLabel("Product Cost:")
-cost_input = QLineEdit()
-
-sell_label = QLabel("Product Selling Price:")
-sell_input = QLineEdit()
-
-stock_label = QLabel("Stock Available:")
-stock_input = QLineEdit()
-
-save_button = QPushButton("Save Product")
-
-
-layout1.addWidget(name_label)
-layout1.addWidget(name_input)
-
-layout1.addWidget(cost_label)
-layout1.addWidget(cost_input)
-
-layout1.addWidget(sell_label)
-layout1.addWidget(sell_input)
-
-layout1.addWidget(stock_label)
-layout1.addWidget(stock_input)
-
-layout1.addWidget(save_button)
-save_button.clicked.connect(add_prod_to_db)
-
-
-tab1.setLayout(layout1)
+        self.stock_label = QLabel("Stock Availabel:")
+        self.stock_input = QLineEdit()
 
 
 
@@ -73,12 +44,84 @@ tab1.setLayout(layout1)
 
 
 
+# app = QApplication(sys.argv)
+# window = QMainWindow()
+
+# window.setWindowTitle("Stainless Shop POS")
+# window.resize(900, 700)
+
+
+# tabs = QTabWidget()
+
+# tab1 = QWidget()
+# tab2 = QWidget()
+# tab3 = QWidget()
+# tab4 = QWidget()
+
+# tabs.addTab(tab1, "Add Item")
+# tabs.addTab(tab2, "New Sale")
+# tabs.addTab(tab3, "Stock List")
+# tabs.addTab(tab4, "Deliveries")
 
 
 
-window.setCentralWidget(tabs)
-window.show()
+# def add_prod_to_db():
+#     name = name_input.text()
+#     cost = cost_input.text()
+#     sell = sell_input.text()
+#     stock = stock_input.text()
+#     print(f"Saved! {name, cost, sell, stock}")
 
-sys.exit(app.exec())
+
+
+# layout1 = QVBoxLayout()
+
+# name_label = QLabel("Product Unit Name:")
+# name_input = QLineEdit()
+
+# cost_label = QLabel("Product Cost:")
+# cost_input = QLineEdit()
+
+# sell_label = QLabel("Product Selling Price:")
+# sell_input = QLineEdit()
+
+# stock_label = QLabel("Stock Available:")
+# stock_input = QLineEdit()
+
+# save_button = QPushButton("Save Product")
+
+
+# layout1.addWidget(name_label)
+# layout1.addWidget(name_input)
+
+# layout1.addWidget(cost_label)
+# layout1.addWidget(cost_input)
+
+# layout1.addWidget(sell_label)
+# layout1.addWidget(sell_input)
+
+# layout1.addWidget(stock_label)
+# layout1.addWidget(stock_input)
+
+# layout1.addWidget(save_button)
+# save_button.clicked.connect(add_prod_to_db)
+
+
+# tab1.setLayout(layout1)
+
+
+
+
+
+
+
+
+
+
+
+# window.setCentralWidget(tabs)
+# window.show()
+
+# sys.exit(app.exec())
 
 
